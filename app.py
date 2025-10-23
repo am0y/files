1c585f9a3fd5b806e921bb46a9550baa6d5a096e
am0y <wizebg403@gmail.com>
2025-10-23 12:46:09 +0000

    ahf

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse, Response
-=======
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
->>>>>>> ebde854573f81887de19b4d4bbf18d1e27a37a7a
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import os
-<<<<<<< HEAD
 import httpx
-=======
->>>>>>> ebde854573f81887de19b4d4bbf18d1e27a37a7a
 
 # Create directory for static files
 static_dir = Path(__file__).parent / "static"
@@ -47,8 +39,6 @@ async def terms():
     html_file = Path("static/terms.html")
     return html_file.read_text()
 
-<<<<<<< HEAD
-
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     upload_url = "https://cdn2.verm.dev/v1/upload"
@@ -60,9 +50,6 @@ async def upload_file(file: UploadFile = File(...)):
         except httpx.RequestError as exc:
             return Response(content=f"Error connecting to upload service: {exc}", status_code=503)
 
-
-=======
->>>>>>> ebde854573f81887de19b4d4bbf18d1e27a37a7a
 if __name__ == "__main__":
     print("File Upload App is running at http://0.0.0.0:8000")
     uvicorn.run(app, host="0.0.0.0", port=8000)
